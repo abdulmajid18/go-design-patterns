1bd0db77166974e5bc475b7dc587bf690563a8ec
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-12-25 20:18:10 +0000

    abstract_factory test

--- creational/abstract_factory/abstract_factory_test.go
+++ creational/abstract_factory/abstract_factory_test.go
@@ -19,3 +19,20 @@ func TestMotorbikeFactory(t *testing.T) {
 	}
 	t.Logf("Sport motorbike has type %d\n", sportBike.GetMotorBikeType())
 }
+
+func TestCarFactory(t *testing.T) {
+	carF, err := BuildFactory(CarFactoryType)
+	if err != nil {
+		t.Fatal(err)
+	}
+	carVehicle, err := carF.Build(LuxuryCarType)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Car vehicle has %d seats\n", carVehicle.NumWheels())
+	luxuryCar, ok := carVehicle.(Car)
+	if !ok {
+		t.Fatal("Struct assertion has failed")
+	}
+	t.Logf("Luxury car has %d doors.\n", luxuryCar.NumDoors())
+}
