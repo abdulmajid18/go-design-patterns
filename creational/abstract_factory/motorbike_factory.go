a5c36a714e478cd5b7749f0aa827adc29bddc790
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-12-25 19:36:23 +0000

    motorbike factory

---
+++ creational/abstract_factory/motorbike_factory.go
@@ -0,0 +1,21 @@
+package abstract_factory
+
+import "fmt"
+
+const (
+	SportMotorbikeType  = 1
+	CruiseMotorbikeType = 2
+)
+
+type MotorbikeFactory struct{}
+
+func (m *MotorbikeFactory) Build(v int) (Vehicle, error) {
+	switch v {
+	case SportMotorbikeType:
+		return new(SportMotorbike), nil
+	case CruiseMotorbikeType:
+		return new(CruiseMotorbike), nil
+	default:
+		return nil, fmt.Errorf("vehicle of ype %d not recognized\n", v)
+	}
+}
