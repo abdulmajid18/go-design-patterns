a346becc08c94ad9c44d116b8ef4813969186cc8
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-12-25 00:48:45 +0000

    factory: implementation

--- creational/factory.go
+++ creational/factory.go
@@ -18,7 +18,7 @@ func GetPaymentMethod(m int) (PaymentMethod, error) {
 	case Cash:
 		return new(CashPM), nil
 	case DebitCard:
-		return new(DebitCardPM), nil
+		return new(CreditCardPM), nil
 	default:
 		return nil, fmt.Errorf("payment method %d not recognized", m)
 	}
@@ -33,4 +33,11 @@ func (c *CashPM) Pay(amount float32) string {
 
 func (c *DebitCardPM) Pay(amount float32) string {
 	return fmt.Sprintf("%0.2f paid using debit card\n", amount)
+}
+
+type CreditCardPM struct{}
+
+func (d *CreditCardPM) Pay(amount float32) string {
+	return fmt.Sprintf("%#0.2f paid using new credit card implementation\n",
+		amount)
 }
