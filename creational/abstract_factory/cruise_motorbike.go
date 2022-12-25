ed6ebbdbeccc08a611c7daeea40f28e381d5469a
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2022-12-25 19:35:03 +0000

    cruise methods

---
+++ creational/abstract_factory/cruise_motorbike.go
@@ -0,0 +1,14 @@
+//cruise_motorbike.go
+package abstract_factory
+
+type CruiseMotorbike struct{}
+
+func (c *CruiseMotorbike) NumWheels() int {
+	return 2
+}
+func (c *CruiseMotorbike) NumSeats() int {
+	return 2
+}
+func (c *CruiseMotorbike) GetMotorbikeType() int {
+	return CruiseMotorbikeType
+}
