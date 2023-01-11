9fe462631f49996abb7d74490c89761a8e6313d3
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-01-11 23:50:15 +0000

    concrete builder

---
+++ creational/builder/builder_2/normalBuilder.go
@@ -0,0 +1,31 @@
+package main
+
+type NormalBuilder struct {
+	windowType string
+	doorType   string
+	floor      int
+}
+
+func newNormalBuilder() *NormalBuilder {
+	return &NormalBuilder{}
+}
+
+func (b *NormalBuilder) setWindowType() {
+	b.windowType = "wooden window"
+}
+
+func (b *NormalBuilder) setDoorType() {
+	b.doorType = "wooden Door"
+}
+
+func (b *NormalBuilder) setNumFloor() {
+	b.floor = 2
+}
+
+func (b *NormalBuilder) getHouse() House {
+	return House{
+		doorType:   b.doorType,
+		windowType: b.windowType,
+		floor:      b.floor,
+	}
+}
